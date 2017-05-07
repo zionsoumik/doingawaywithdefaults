d279cd4a9c033ca4934f812533135adf64466117
malancas <malancaster0@gmail.com>
2017-05-07 00:31:06 -0400

    Removed unused imports, changed functions to use new main format

--- NDmain.py
+++ NDmain.py
@@ -1,55 +1,68 @@
-from NDChild import NDChild
-from time import clock
 from random import choice
-import csv
-from datetime import datetime
-import os
-from sys import argv
 from argparse import ArgumentParser
+from argparse import ArgumentParser
+from NDresults import NDresults
+from NDChild import NDChild
 from Sentence import Sentence
-import NDresults
 
 #GLOBALS
 rate = 0.02
 conservativerate = 0.01
-numberofsentences = 200000
+#numberofsentences = 200000
 threshold = .001
 
-#language = "611" #English
-#language = "584" #French
-#language = "2253" #German
-language = "3856" #Japanese
-
 infoFile = open('EngFrJapGerm.txt','rU')
-
-LD = []
 
 def pickASentence(languageDomain):
     return choice(languageDomain)
 
-def createLD():
+def createLD(language):
+    LD = []
     for line in infoFile:
         [grammStr, inflStr, sentenceStr] = line.split("\t")
         sentenceStr = sentenceStr.rstrip()
-        s = Sentence([grammStr, inflStr, sentenceStr]) #constructor creates sentenceList
+        # constructor creates sentenceList
+        s = Sentence([grammStr, inflStr, sentenceStr])
         if grammStr == language:
             LD.append(s)
+    return LD
 
 if __name__ == '__main__':
-    createLD()
+    # The argument keeps track of the mandatory arguments,
+    #number of learners, max number of sentences, and target grammar
+    parser = ArgumentParser(prog='Doing Away With Defaults', description='Set simulation parameters for learners')
+    parser.add_argument('integers', metavar='int', type=int, nargs=3,
+                        help='(1) The number of learners (2) The number of '
+                         'sentences consumed (3) The target grammar\'s code '
+                          '(English=611, German=2253, French=584, Japanese=3856)')
 
-    ndr = NDresults.NDresults()
-    ndr.writeOutputHeader(611, 1, numberofsentences)
+    args = parser.parse_args()
+    numLearners = 0
+    maxSentences = 0
 
-    aChild = NDChild(rate, conservativerate)
+    # Test whether certain command line arguments
+    # can be converted to positive integers
+    numLearners = args.integers[0]
+    numberofsentences = args.integers[1]
+    language = args.integers[2]
+    if numLearners < 1 or numberofsentences < 1:
+        print('Arguments must be positive integers')
+        sys.exit(2)
 
-    for i in range(numberofsentences):
-        s = pickASentence(LD)
-        aChild.consumeSentence(s)
-        ndr.checkIfParametersMeetThreshold(threshold, aChild.grammar, i)
+    ndr = NDresults()
+    ndr.writeOutputHeader(language, numLearners, numberofsentences)
 
-    ndr.writeResults(aChild.grammar, i)
+    for i in range(numLearners):
+        LD = createLD(str(language))
+        print(LD)
+        aChild = NDChild(rate, conservativerate)
 
-    print aChild.grammar
+        for j in range(numberofsentences):
+            s = pickASentence(LD)
+            aChild.consumeSentence(s)
+            ndr.checkIfParametersMeetThreshold(threshold, aChild.grammar, j)
+
+        ndr.writeResults(aChild.grammar, i)
+        print aChild.grammar
 
     infoFile.close()
